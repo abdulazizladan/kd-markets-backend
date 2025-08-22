f368640ab10d5a83b6db309d9b5dc9a334dae879
abdulazizladan <abdulazizladan@gmail.com>
2025-08-22 06:15:12 +0100

    fix: Fix sqlite constraint issue on user entity

--- src/user/entities/info.entity.ts
+++ src/user/entities/info.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
 import { IsString, IsOptional, IsNumber } from "class-validator";
 
@@ -7,7 +7,7 @@ export class Info {
     /**
      * Unique identifier for the info record (Primary Key)
      */
-    @Column({ primary: true, generated: true })
+    @PrimaryGeneratedColumn('uuid')
     @IsNumber()
     id: string;
     
